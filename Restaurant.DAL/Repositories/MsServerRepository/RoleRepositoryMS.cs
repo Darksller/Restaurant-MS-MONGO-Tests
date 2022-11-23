6ba0d81d55a2c123bcb6ebd5023750271496d46e
Darksller K <dar4ikdenis@gmail.com>
2022-11-23 09:03:12 +0300

    CRUD implementation

--- Restaurant.DAL/Repositories/MsServerRepository/RoleRepositoryMS.cs
+++ Restaurant.DAL/Repositories/MsServerRepository/RoleRepositoryMS.cs
@@ -2,37 +2,118 @@
 using Restaurant.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Restaurant.DAL.Repositories.MsServerRepository
 {
-    internal class RoleRepositoryMS : IRoleRepository
+    public class RoleRepositoryMS : IRoleRepository
     {
+
+        private readonly string _connectionString;
+
+        public RoleRepositoryMS(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
         public bool Create(Role entity)
         {
-            throw new NotImplementedException();
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                string query = $"INSERT INTO roles VALUES (@name)";
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    cmd.Parameters.Add("@name", SqlDbType.NChar).Value = entity.Name;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            return true;
         }
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                string query = $"DELETE FROM roles WHERE _id = @id";
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            return true;
         }
 
         public Role Get(int id)
         {
-            throw new NotImplementedException();
+            Role role = new Role();
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                string query = $"SELECT * FROM roles WHERE _id = @id";
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            role.Id = (int)reader["_id"];
+                            role.Name = (string)reader["name"];
+                        }
+
+                    }
+                }
+            }
+            return role;
         }
 
         public IEnumerable<Role> GetAll()
         {
-            throw new NotImplementedException();
+            List<Role> roles = new List<Role>();
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                string query = $"SELECT * FROM roles";
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            roles.Add(new Role
+                            {
+                                Id = (int)reader["_id"],
+                                Name = (string)reader["name"]
+                            });
+                        }
+
+                    }
+                }
+            }
+            return roles;
         }
 
-        public bool Update(int id)
+        public bool Update(Role entity)
         {
-            throw new NotImplementedException();
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                string query = $"UPDATE roles SET name = @name WHERE _id = @id";
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    cmd.Parameters.Add("@id", SqlDbType.Int).Value = entity.Id;
+                    cmd.Parameters.Add("@name", SqlDbType.NChar).Value = entity.Name;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            return true;
         }
     }
 }
