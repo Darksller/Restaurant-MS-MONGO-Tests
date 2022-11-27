8c90740704b60f51fe77be2dc6ccd582ba299a15
Darksller K <dar4ikdenis@gmail.com>
2022-11-27 22:29:44 +0300

    update

---
+++ Restaurant.Domain/Models/DishIngredient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Domain.Models
+{
+    public class DishIngredient
+    {
+        public int Id { get; set; }
+
+        public int idDish { get; set; }
+
+        public int idIngredient { get; set; }
+
+    }
+}
---
+++ Restaurant.Domain/Models/OrderDish.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Domain.Models
+{
+    public class OrderDish
+    {
+        public int Id { get; set; }
+
+        public int idDish { get; set; }
+
+        public int idOrder { get; set; }
+
+        public int Count { get; set; }
+    }
+}
